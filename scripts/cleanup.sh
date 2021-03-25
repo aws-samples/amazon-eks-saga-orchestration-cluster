efab6715c040b7385924e3f27b9e4cb45db62154
nsubrahm <nsubrahm@amazon.com>
2021-03-25 15:54:09 +0530

    Changed stack name.

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -31,7 +31,7 @@ remove_cluster() {
   EKS_CLUSTER=$1
 
   echo 'Detaching policy from node IAM role'
-  STACK_NAME=eksctl-${EKS_CLUSTER}-nodegroup-ng-db
+  STACK_NAME=eksctl-${EKS_CLUSTER}-cluster
   ROLE_NAME=$(aws cloudformation describe-stack-resources --stack-name $STACK_NAME | jq -r '.StackResources[] | select(.ResourceType=="AWS::IAM::Role") | .PhysicalResourceId')
   aws iam detach-role-policy --role-name $ROLE_NAME --policy-arn arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy
   echo 'Removing cluster eks-saga-orchestration'
