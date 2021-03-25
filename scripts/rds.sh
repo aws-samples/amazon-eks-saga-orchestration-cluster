6bb3039fdc0cb826cf98d56744371ca171e75c54
nsubrahm <nsubrahm@amazon.com>
2021-03-25 09:18:15 +0530

    Attached new SG to RDS.

--- scripts/rds.sh
+++ scripts/rds.sh
@@ -9,7 +9,7 @@ r_setup() {
   RDS_VPC=$3
   RDS_DB_ID=$4
 
-  RDS_SG=`aws rds describe-db-instances --db-instance-identifier ${RDS_DB_ID} --query 'DBInstances[0].VpcSecurityGroups[0].VpcSecurityGroupId' --output text`
+  RDS_SG=`aws ec2 create-security-group --description "EKS Saga DB SG" --group-name "eks-saga-db-sg" --vpc-id ${RDS_VPC} --query 'GroupId' --output text`
   SUBNETS=(`aws cloudformation describe-stack-resources --stack-name $STACK_NAME | jq -r '.StackResources[] | select((.ResourceType=="AWS::EC2::Subnet") and (.LogicalResourceId | startswith("SubnetPrivate"))) | .PhysicalResourceId'`)
   
   for s in "${SUBNETS[@]}"
@@ -17,6 +17,8 @@ r_setup() {
     CIDR_BLOCK=`aws ec2 describe-subnets --subnet-ids ${s} --query 'Subnets[0].CidrBlock' --output text`
     aws ec2 authorize-security-group-ingress --group-id ${RDS_SG} --protocol tcp --port 3306 --cidr ${CIDR_BLOCK}
   done
+
+  aws rds modify-db-instance --db-instance-identifier ${RDS_DB_ID} --vpc-security-group-ids ${RDS_SG}
 
   echo "${RDS_SG} in RDS VPC ${RDS_VPC} updated to allow MySQL traffic from EKS VPC ${EKS_VPC}"
 }
