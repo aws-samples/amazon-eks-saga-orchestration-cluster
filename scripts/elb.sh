107d4dd3a17d3d15ab9ca162e733112a18d165b3
nsubrahm <nsubrahm@amazon.com>
2021-04-07 11:07:58 +0530

    ALB Controller update
    - Set `serviceAccount` name in `helm` command.

--- scripts/elb.sh
+++ scripts/elb.sh
@@ -6,7 +6,7 @@ e_setup() {
   ACCOUNT_ID=$1
 
   helm repo add eks https://aws.github.io/eks-charts
-  helm install aws-load-balancer-controller eks/aws-load-balancer-controller --set clusterName=eks-saga-orchestration --set nodeSelector.role=web --set serviceAccount.create=false -n kube-system
+  helm install aws-load-balancer-controller eks/aws-load-balancer-controller --set clusterName=eks-saga-orchestration --set nodeSelector.role=web --set serviceAccount.create=false --set serviceAccount.name=aws-load-balancer-controller -n kube-system
 }
 
 if [[ $# -ne 1 ]] ; then
